437d1dfe44defb848bb20188c21438a9ddd8ec8c
BrainzoRR <134707147+BrainzoRR@users.noreply.github.com>
2026-03-09 16:48:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Settings(BaseSettings):
     bot_token: str
-    database_url: str = "postgresql+asyncpg://dota2fm:pass@localhost/dota2fm"
+    database_url: str = "postgresql://bothost_db_fb7fd4a9fe82:Lq_YVEBqJNRR932WuY2ho4fV7otNcUnMuT6WXCLXZ8M@node1.pghost.ru:32803/bothost_db_fb7fd4a9fe82"
     redis_url: str = "redis://localhost:6379/0"
     admin_ids: List[int] = []
     debug: bool = False
